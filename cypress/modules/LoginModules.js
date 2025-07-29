6a0ebb61ca6841a10dbffd7bab20f7e914695524
Javier Vasquez C. <javiandriu21@gmail.com>
2025-07-29 17:20:37 -0500

    Added login modules file

---
+++ cypress/modules/LoginModules.js
@@ -0,0 +1,18 @@
+class LoginModules {
+
+    get emailLoginInput () {
+        return cy.get('[data-qa="login-email"]')
+    }
+
+    get passwordLoginInput () {
+        return cy.get('[data-qa="login-password"]')
+    }
+
+    get loginButton () {
+        return cy.get('[data-qa="login-button"]')
+    }
+
+   
+
+}
+export default LoginModules;
